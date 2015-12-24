04d519664d1512120013079af430d9ae926a60bf
Malaz Alamir <malaz.alamir@hotmail.com>
2015-12-24 14:06:09 -0500

    Validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,10 @@
 class Product < ActiveRecord::Base
+  validates :title, :description, :image_url, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :title, uniqueness: true
+  VALID_IMAGE_URL = %r{\.(gif|jpg|png)\Z}i
+  validates :image_url, allow_blank: true, format: {
+    with: VALID_IMAGE_URL,
+    message: 'must be a URL for GIF, JPG or PNG image.'
+  }
 end
