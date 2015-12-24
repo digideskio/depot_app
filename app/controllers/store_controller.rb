555dcec95e3babdfcc0aeb739bbdb5df5f64a4fb
Malaz Alamir <malaz.alamir@hotmail.com>
2015-12-24 18:49:59 -0500

    improved cart

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,13 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+    count = increment_count
+    @message = "You've been here #{count} times" if session[:counter] > 5
+  end
+  
+  private
+  def increment_count
+    session[:counter] ||= 0
+    session[:counter] += 1
   end
 end
